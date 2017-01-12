eab4735807be046a307f18aa7442f5950889d5da
Boris Burtin <boris@brkt.com>
2017-01-12 15:46:43 -0800

    Update copyright
    Update copyright blocks to say 2017.

--- brkt_cli/aws/test_aws.py
+++ brkt_cli/aws/test_aws.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Bracket Computing, Inc. All Rights Reserved.
+# Copyright 2017 Bracket Computing, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
