fe143bc4b37456336aa4c5c0e6b8bef4cf97c112
apoorva <apoorva@brkt.com>
2017-05-01 14:13:54 -0700

    Fix static ip configuration for wrap script
    1. Fix static IP configuration for wrap script.
    2. Fix VM reconfiguration for wrap script.

--- brkt_cli/esx/wrap_image.py
+++ brkt_cli/esx/wrap_image.py
@@ -50,7 +50,10 @@ def wrap_from_s3(vc_swc, guest_vmdk, vm_name=None,
         vc_swc.add_disk(vm, filename=guest_vmdk_path, unit_number=1)
         if user_data_str:
             vc_swc.send_userdata(vm, user_data_str)
+        vc_swc.reconfigure_vm_cpu_ram(vm)
         if static_ip:
+            vc_swc.power_on(vm)
+            vc_swc.power_off(vm)
             vc_swc.configure_static_ip(vm, static_ip)
         vc_swc.power_on(vm)
         ip_addr = vc_swc.get_ip_address(vm)
@@ -84,7 +87,10 @@ def wrap_from_local_ovf(vc_swc, guest_vmdk, vm_name=None,
         vc_swc.add_disk(vm, filename=guest_vmdk_path, unit_number=1)
         if user_data_str:
             vc_swc.send_userdata(vm, user_data_str)
+        vc_swc.reconfigure_vm_cpu_ram(vm)
         if static_ip:
+            vc_swc.power_on(vm)
+            vc_swc.power_off(vm)
             vc_swc.configure_static_ip(vm, static_ip)
         vc_swc.power_on(vm)
         ip_addr = vc_swc.get_ip_address(vm)
@@ -115,7 +121,10 @@ def wrap_from_vmdk(vc_swc, guest_vmdk, vm_name=None,
         vc_swc.add_disk(vm, filename=guest_vmdk_path, unit_number=1)
         if user_data_str:
             vc_swc.send_userdata(vm, user_data_str)
+        vc_swc.reconfigure_vm_cpu_ram(vm)
         if static_ip:
+            vc_swc.power_on(vm)
+            vc_swc.power_off(vm)
             vc_swc.configure_static_ip(vm, static_ip)
         vc_swc.power_on(vm)
         ip_addr = vc_swc.get_ip_address(vm)
