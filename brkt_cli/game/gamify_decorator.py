5729053b97799223bac22a7f880e996077cb2363
Adam Tonderski <atonderski@brkt.com>
2017-06-09 12:29:41 -0700

    First version of duck_hunt

--- brkt_cli/game/gamify_decorator.py
+++ brkt_cli/game/gamify_decorator.py
@@ -40,11 +40,6 @@ def gamify(func):
             sys.stdout = sys.stderr = log_file
             func(*args, **kwargs)
 
-        print "Game will start in:"
-        for i in reversed(range(1)):
-            print i + 1
-            time.sleep(1)
-
         p_cli = multiprocessing.Process(target=special_func,
                                         args=args,
                                         kwargs=kwargs)
@@ -67,6 +62,7 @@ def gamify(func):
                 #     with open(TMP_LOG_FILE, 'a') as log_file_append:
                 #         log_file_append.write('Command is done!')
                 if p_cli.exitcode is not None and p_game.exitcode is not None:
+                    print ''.join(log_file.readlines()),
                     break
                 time.sleep(0.1)
 
