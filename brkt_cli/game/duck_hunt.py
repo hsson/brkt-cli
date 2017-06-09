257b85bf4f10cf9ef789082f973835b1d17e6f64
Adam Tonderski <atonderski@brkt.com>
2017-06-09 13:04:39 -0700

    Kinda working version of duckhunt :)

--- brkt_cli/game/duck_hunt.py
+++ brkt_cli/game/duck_hunt.py
@@ -1,12 +1,15 @@
 import random
 import time
 
-from asciimatics.effects import Effect
+from asciimatics.effects import Effect, Sprite
 from asciimatics.exceptions import NextScene
-from asciimatics.particles import Rain
-from asciimatics.paths import Path
+from asciimatics.particles import Rain, Explosion
+from asciimatics.paths import Path, DynamicPath
 from asciimatics.scene import Scene
 from asciimatics.sprites import Arrow
+from asciimatics.renderers import StaticRenderer
+from asciimatics.screen import Screen
+from asciimatics.event import MouseEvent
 
 import brkt_cli.game
 
@@ -16,6 +19,54 @@ MAX_MISSED_DUCKS = 5
 class DuckHuntStats():
     missed_ducks = 0
     hit_ducks = 0
+
+
+
+class MouseController(DynamicPath):
+    def __init__(self, sprite, screen, x, y):
+        super(MouseController, self).__init__(screen, x, y)
+        self._sprite = sprite
+
+    def process_event(self, event):
+        if isinstance(event, MouseEvent):
+            self._x = event.x
+            self._y = event.y
+            if event.buttons & MouseEvent.DOUBLE_CLICK != 0:
+                # Try to whack the other sprites when mouse is clicked
+                self._sprite.shoot(powerful=True)
+            elif event.buttons & MouseEvent.LEFT_CLICK != 0:
+                # Try to whack the other sprites when mouse is clicked
+                self._sprite.shoot(powerful=False)
+            else:
+                return event
+        else:
+            return event
+
+class Gun(Sprite):
+    def __init__(self, screen):
+        """
+        See :py:obj:`.Sprite` for details.
+        """
+        super(Gun, self).__init__(
+            screen,
+            renderer_dict={
+                "default": StaticRenderer(images=["X"])
+            },
+            path=MouseController(
+                self, screen, screen.width // 2, screen.height // 2),
+            colour=Screen.COLOUR_RED)
+        self._screen = screen
+
+    def shoot(self, powerful=True):
+        x, y = self._path.next_pos()
+        for duck in [e for e in self._scene._effects if isinstance(e, Duck)]:
+            try:
+                if self.overlaps(duck, use_new_pos=True):
+                    duck.get_shot()
+            except TypeError:
+                # Probably compared against an unitialized sprite
+                pass
+        self._scene.add_effect(Explosion(self._screen, x, y, 25))
 
 
 class Duck(Arrow):
@@ -28,6 +79,10 @@ class Duck(Arrow):
             DuckHuntStats.missed_ducks += 1
             self.delete_count = 1
         super(Duck, self)._update(frame_no)
+
+    def get_shot(self):
+        DuckHuntStats.hit_ducks += 1
+        self.delete_count = 1
 
 
 class DuckSpawner(Effect):
@@ -47,7 +102,7 @@ class DuckSpawner(Effect):
         path.jump_to(self._screen.width * random.randint(0, 1),
                      self._screen.height)
         target = int(self._screen.width * random.random())
-        path.move_straight_to(target, -10, 40)
+        path.move_straight_to(target, -10, 100)
         self._scene.add_effect(Duck(self._screen, path))
         self.last_spawn_time = time.time()
 
@@ -75,6 +130,7 @@ def get_scenes(screen):
     # MAIN GAME
     effects = [
         DuckSpawner(screen, 1),
+        Gun(screen),
         Rain(
                 screen,
                 9999999
